2ea312a9669a7668ad843c33912870801d3a3ced
John Holland <thomas.holland.john@gmail.com>
2015-12-31 17:53:09 -0500

    created base project

---
+++ AssignmentDemo/TemperatureHelpers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentDemo
+{
+    public static class TemperatureHelpers
+    {
+        public static double ConvertFromCelsiusToFarenheit(double inputDegrees)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static double ConvertFromFarenheitToCelsius(double inputDegrees)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
